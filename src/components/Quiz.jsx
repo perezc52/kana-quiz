32aef194d39c7c64128b6d5c004972fca8379b05
Carlos Perez <pcarlos132@yahoo.com>
2023-03-09 19:02:06 -0600

    worked on quiz component
    added font
    kana is now shuffled on start, and after options are changed.
    added buttons
    added empty functions for the buttons.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,21 +2,35 @@ import { useState, useEffect } from "react"
 
 function Quiz(props) {
     const [quizKana, setQuizKana] = useState([])
+    const [message, setMessage] = useState("")
 
     useEffect(() => {
-        setQuizKana(props.data?.map((el,i) => (
-            <li key={i}>{el.kana}</li>
-        )));
+        setQuizKana(shuffleArray(props.data));
     }, [props.data])
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+      }
+
+      function checkAnswer() {
+
+      }
+
+      function nextKana() {
+
+      }
  
     return (
         <div className="quiz">
-            <h1 className="kana"></h1>
-            <ul>
-                {quizKana}
-            </ul>
+            <h1 className="kana">{quizKana[0]?.kana}</h1>
             <input type="text" />
-            <button>Enter</button>
+            <button>I don't know 😩</button>
+            <button onClick={checkAnswer}>Check answer</button>
+            <h2>{message}</h2>
         </div>
     )
 }
